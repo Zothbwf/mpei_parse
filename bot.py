3b50e5ea069fad65103ee8e0fffe6878cb11a0b9
Zothbwf <ikpliskin007@gmail.com>
2025-07-23 15:16:09 +0300

    added possibility to change update interval

--- bot.py
+++ bot.py
@@ -2,20 +2,23 @@ import asyncio
 import logging
 import sys
 from os import environ
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, F, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 from dotenv import load_dotenv
 import json
 from svo import get_places, user_friendly_data, get_data_file, update_data_file
 from kb import main_kb
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import datetime
 
 load_dotenv()
 token = environ.get("token")
 allowed_ids = json.loads(environ.get("allowed_ids"))
+admins = allowed_ids = json.loads(environ.get("admins"))
 urls = json.loads(environ.get("urls"))
 ids = json.loads(environ.get("ids"))
 dp = Dispatcher()
@@ -61,8 +64,22 @@ async def echo_handler(message: Message) -> None:
                 await message.answer(
                     f"{output}", reply_to_message_id=message.message_id
                 )
+            elif "изменить интервал" in msg and message.from_user.id in admins:
+                try:
+                    splitted_msg = msg.split()
+                    logging.info(dp['job'])
+                    if len(splitted_msg) == 3:
+                        await change_interval(dp["job"], int(splitted_msg[2]))
+                        message.answer(
+                            "Все заебок",
+                        )
+                except Exception as e:
+                    logging.critical(e)
             else:
-                await message.answer("Воспользуйтесь кнопками снизу",reply_markup=main_kb(message.from_user.id))
+                await message.answer(
+                    "Воспользуйтесь кнопками снизу",
+                    reply_markup=main_kb(message.from_user.id),
+                )
 
     except Exception as e:
         logging.error(e)
@@ -77,23 +94,37 @@ async def data_updater():
 async def setup_scheduler():
     scheduler = AsyncIOScheduler()
 
-    scheduler.add_job(data_updater, "interval", minutes=30)
-
+    job = scheduler.add_job(data_updater, "interval", minutes=30)
     scheduler.start()
+    return job
+
+
+async def change_interval(job, interval):
+    job.reschedule("interval", minutes=interval)
+    job.modify(next_run_time=datetime.datetime.now())
 
 
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp.startup.register(setup_scheduler)
+    job = await setup_scheduler()
+    dp["job"] = job
+    
     # And the run events dispatching
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,handlers=[logging.FileHandler('my.log',encoding='UTF-8'), logging.StreamHandler()],
-    format="%(asctime)s %(levelname)s %(message)s")
-    
-    try:   
+    logging.basicConfig(
+        level=logging.INFO,
+        handlers=[
+            logging.FileHandler("my.log", encoding="UTF-8"),
+            logging.StreamHandler(),
+        ],
+        format="%(asctime)s %(levelname)s %(message)s",
+    )
+
+    try:
         asyncio.run(main())
     except Exception as e:
         logging.critical(e)
